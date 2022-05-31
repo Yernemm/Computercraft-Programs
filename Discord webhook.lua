ea14ce43f9808fab132a3ff46bb70a4e368b4a86
Yernemm <yrnemmizer@gmail.com>
2022-05-31 14:40:49 +0100

    this works lmao

--- Discord webhook.lua
+++ Discord webhook.lua
@@ -1,5 +1,11 @@
+local webhook = "WEBHOOK"
+
 while true do
     event, username, message = os.pullEvent("chat") -- Will be fired when someone sends a chat message
     --print(username.. " just wrote: ".. message) -- Prints "*User* just wrote: *Message*"
-    http.post()
+
+    local postData = '{"content": "' .. message .. '", "username": "' .. username .. '"}'
+    print(postData)
+    local p = http.post(webhook, postData, {["Content-Type"] = "application/json"})
+    print(p.readAll())
   end
